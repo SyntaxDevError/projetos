99ee343cae859488252dca194490e9b0a3842023
SyntaxDevError <f.batistasants016@gmail.com>
2024-06-26 09:11:14 -0300

    Nova atualização de main

--- main.py
+++ main.py
@@ -3,12 +3,67 @@ from random import randint
 from time import sleep
 from tkinter import messagebox
 
+def dados_game():
+    def manual():
+        messagebox.showinfo("Manual de instruções",""" Digite no campo de entrada um valor situado entre 1 e 6
+                   Caso informe um valor nulo, palavra ou caracter específico dará erro no jogo
+                   Logo após aperte o botão play e o jogo funcionara caso esteja nas condições ideais
+                   A cada rodada será ranomizado um valor novamente, ou seja, a cada play mudara o valor do cpu
+                   divirta-se""")
+    
+    def palp():
+        try:
+            valor = campo_entrada.get()
+            valor1 = valor.replace(" ", "")
+            valor2 = int(valor1)
+        except TypeError as tipo:
+            messagebox.showerror("Error", "Digite um número inteiro")
+        except:
+            messagebox.showerror("Error", "Erro ao digitar o valor")
+        else:
+            if valor2 != '':
+                if 0 < valor2 < 7 :
+                    campo.config(text='Calculando resultado...')
+                    play.config(state="disabled")
+                    dadosJan.update()
+                    dadosJan.after(3000)
+                    play.config(state='active')
+                    cpu = randint(1, 6)
+                    if valor2 == cpu:
+                        campo.config(text=f"Acertou!!\nO computador jogou {cpu}\nO seu palpite foi {valor2}  ")
+                    else:
+                        campo.config(text=f'Errou...\nO computador jogou {cpu}\nO seu palpite foi {valor2}')
+                else:
+                    messagebox.showerror("valor número", "Digite um número entre 1 e 6!")
+        
+
+
+    dadosJan = Tk()
+    dadosJan.title("jogo dos dados")
+    dadosJan.geometry("460x380")
+
+    butback = Button(dadosJan, text='<--', fg='white', bg='red', command=lambda: dadosJan.destroy()).grid(column=0, row=0)
+    butinst = Button(dadosJan, text='Instruções', bg='blue', fg='white', command=manual).grid(column=1, row=0)
+
+    campo = Label(dadosJan, text='Jogo dos dados',width=20 ,height=6 , font=('roboto', 15))
+    campo.grid(column=0, row=1, columnspan=2)
+
+
+    text_label = Label(dadosJan, text="Digite seu palpite entre 1 a 6:", font=('Roboto', 10, 'bold')).grid(column=0, row=2, columnspan=2, sticky='N')
+    campo_entrada = Entry(dadosJan, width=20, font=('roboto', 20, 'bold'))
+    campo_entrada.grid(column=0, row=3, columnspan=2, sticky="W")
+    play = Button(dadosJan, text='Play', command=palp, width=12, height=2)
+    play.grid(column=0, row=4, sticky='W')
+
+
+    dadosJan.mainloop()
+
 
 def jokenpo():
     def contagem():
         campo.config(text=f'Calculando resultado...')
         jan_jokenpo.update()
-        jan_jokenpo.after(5000)
+        jan_jokenpo.after(3000)
         
     def pedra():
         contagem()
@@ -89,8 +144,10 @@ def jokenpo():
 janela_principal = Tk()
 janela_principal.title('Jogos vc computador...')
 
-close = Button(janela_principal, text="X", fg='white', bg='red', command=lambda: janela_principal.destroy()).grid(column=0, row=0, sticky="w")
-ppt = Button(janela_principal, text='JOKENPO', command=jokenpo).grid(column=0, row=1)
+larg, alt = 20, 4
 
+close = Button(janela_principal, text="X", fg='white', bg='red', command=lambda: janela_principal.destroy()).grid(column=0, row=0, sticky="w")
+ppt = Button(janela_principal, text='JOKENPO', command=jokenpo, width=larg, height=alt).grid(column=0, row=1, pady=20)
+jogo_dados = Button(janela_principal, text="Jogo dos dados", command=dados_game, width=larg, height=alt).grid(column=0, row=2, pady=20)
 
 janela_principal.mainloop()
